ff14d6a767f567c768fe262669ef59a647c2d441
nelsonkimaiga <nelsonkimaiga@aol.com>
2018-08-17 13:13:09 +0300

    Add basic db connection script

--- index.php
+++ index.php
@@ -6,4 +6,23 @@
  * Time: 13:02
  */
 
-echo 'Hello World!';
+//using mysqli to initiate db connection
+
+$servername = "localhost";
+$database = "zilojo";
+$username = "root";
+$password = "303seminarian";
+
+// Creating connection
+
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+
+if (!$conn) {
+
+    die("Connection failed: " . mysqli_connect_error());
+
+}
+echo "Connected successfully";
+mysqli_close($conn);
